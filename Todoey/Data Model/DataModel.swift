af3c589e0e594da0a6058f3cbfe27fb6f22b12a2
Silvia Susini <susinisilvia@tiscali.it>
2018-12-29 17:01:02 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/DataModel.swift
@@ -0,0 +1,13 @@
+//
+//  DataModel.swift
+//  Todoey
+//
+//  Created by Silvia Susini on 29/12/2018.
+//  Copyright © 2018 Silvia Susini. All rights reserved.
+//
+
+import Foundation
+class DataModel{
+    var itemArray = ["Find Mike","Buy Eggs","Destroy Demogorgon"]
+    var isCheckedArray : [Bool] = [true]
+}
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Silvia Susini on 29/12/2018.
+//  Copyright © 2018 Silvia Susini. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title : String = ""
+    var done : Bool = false
+}
